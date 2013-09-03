d1a0b57c2f4e420c070178ef349b78b898f5db0d
bakaburg1 <bakaburg1@gmail.com>
2013-09-03 02:01:55 +0200

    Added logic for pertinency quick toggle for resources

--- js/admin.js
+++ js/admin.js
@@ -25,7 +25,58 @@ jQuery(document).ready(function ($) {
 		$('.wrap #icon-index').addClass('icon-home');
 	};
 
+	opbg.quickedit_resource_pertinency = function() {
+		if ($('#the-list').length !== 0){
+			console.log('Resources list page');
+
+			$('#the-list .row-actions').append(' | <span class="pertinency-toggle"><a href="#" title="change pertinency" alt="change pertinency">Set as not pertinent</a></span>');
+		
+			$('#the-list').on('click.quickedit_resource_pertinency', '.pertinency-toggle a', function() {
+				$this = $(this);
+
+				if ($this.hasClass('disabled')) return false;
+
+				$this.addClass('disabled');
+
+				$this.parent().append(' <i class="icon-spin icon-refresh"></i>');
+
+				var loading = $this.siblings('i');
+
+				var status_item = $this.closest('tr').find('td:contains("New"), td:contains("Categorized"), td:contains("Not pertinent")');
+
+				var resource_id = $(this).closest('tr').attr('id');
+				resource_id = window.parseInt(resource_id.replace('item-', ''));
+
+				$.ajax({
+					type: 'post',
+					url: window.ajaxurl,
+					data: {
+						action: 'quickedit_resource_pertinency',
+						nonce: resource_list_page_js_objects.pertinency_quickedit_nonce,
+						resource_id: resource_id
+					},
+					success: function(response){
+						if (response !== false){
+							status_item.text('Not pertinent');
+						}
+					},
+					complete: function(){
+						loading.remove();
+						$this.removeClass('disabled');
+					}
+				});
+
+				return false;
+
+			});
+		}
+	};
+
 	opbg.set_admin_menu_icons();
+
+	if (pagenow === "toplevel_page_pods-manage-resources"){
+		opbg.quickedit_resource_pertinency();
+	}
 
 	// Convert urls in pods list pages table into links
     if ( $('.wp-list-table').length ){
