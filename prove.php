48c2bfb65975b80a65068da8525b6fa327061789
bakaburg1 <bakaburg1@gmail.com>
2013-11-07 10:26:42 +0100

    various

--- prove.php
+++ prove.php
@@ -1,7 +1,7 @@
 start
 <pre>
 
-<?php
+	<?php
 /**
  * Template Name: prova
  *
@@ -27,11 +27,31 @@ start
 	asort($results);
 	print_r($results);
 	*/
-	
+
+	$resources = pods('resources')->find(array('limit' => -1));
+
+	$results = array();
+
+	$i = 0;
+
+	while($resources->fetch()){
+		$url = $resources->field('url');
+
+		$response = wp_remote_get($url);
+
+		$the_body = wp_remote_retrieve_body($response);
+
+		if (preg_match_all("/gravidanz|preconcezional|prenatal|concepimento/i", $the_body) === 0) {
+			$i++;
+		}
+	}
+
+	echo "Resources with no matches: ".$i;
+
 	delete_option('resource_filtering_status');
 	delete_option('filtered_in_resources');
 	delete_option('filtered_out_resources');
-?>
+	?>
 
 
 </pre>
