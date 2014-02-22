f2a89898e0e84221ba06dcc1a4d9528ab2a51e29
bakaburg1 <bakaburg1@gmail.com>
2014-02-22 18:45:25 +0100

    Bug Fixing and some features
    - Now resources data are not fetched if one of the date inputs is not
    correct (wrong or empty)
    - Code refactoring
    - Date inputs are rembered in browser cache and repopulated on page
    reload

--- js/admin_dashboard.js
+++ js/admin_dashboard.js
@@ -1,8 +1,77 @@
 jQuery(document).ready(function ($) {
 
-	//console.log('admin_dashboard.js called');
+	/**** GLOBAL VARS INIT ****/
 
 	window.dashboard_summary_widget = $('#dashboard_summary');
+
+	var dashboard_summary_period = localStorage.getItem('dashboard_summary_period') || "total"; // load visualization period from browser cache
+
+	var are_results_being_called = false;
+
+	/**** FUNCTIONS ****/
+
+	opbg.set_datepickers = function(target) {
+
+		var from = moment(dashboard_summary_widget.find('.status-data-range .datepicker-box[data-box = from]').val());
+		var to = moment(dashboard_summary_widget.find('.status-data-range .datepicker-box[data-box = to]').val());
+		var changed = $(target).data("box");
+
+		console.log(from.format());
+		console.log(to.format());
+
+		if (from.format() === "Invalid date" || to.format() === "Invalid date"){
+			return false;
+			/*from = to = moment();
+			dashboard_summary_widget.find('.status-data-range .datepicker-box[data-box = from]').val(from.format('YYYY-MM-DD'));
+			dashboard_summary_widget.find('.status-data-range .datepicker-box[data-box = to]').val(to.format('YYYY-MM-DD'));*/
+		}
+
+		if (changed === "from" ){
+			if (from.isAfter(to)){
+				from = moment(to);
+				dashboard_summary_widget.find('.status-data-range .datepicker-box[data-box = from]').val(from.format('YYYY-MM-DD'));
+			}
+
+		}
+		if (changed === "to"){
+			if (to.isBefore(from)){
+				to = moment(from);
+			}
+			else if (to.isAfter(moment())){
+				to = moment();
+			}
+
+			dashboard_summary_widget.find('.status-data-range .datepicker-box[data-box = to]').val(to.format('YYYY-MM-DD'));
+		}
+
+		localStorage.setItem( 'dashboard_summary_period', from.format('YYYY-MM-DD')+" "+to.format('YYYY-MM-DD') );
+
+		dashboard_summary_period = from.format('YYYY-MM-DD')+" "+to.add('days', 1).format('YYYY-MM-DD');
+
+		opbg.get_dashboard_summary();
+	};
+
+	opbg.dashboard_summary_get_statistics = function() {
+
+        var results     = {};
+        var classes     = [];
+
+		window.dashboard_summary_widget.find('.status-table .key').each(function(){
+			classes.push($(this).attr('class').replace(/ key/g, ''));
+		});
+
+		$(classes).each(function(){
+			var value = window.dashboard_summary_widget.find('.status-table .'+this+'.value').data('status-value');
+
+			//results[this] = !isNaN(window.parseInt(value)) ? window.parseInt(value) : value;
+
+			results[this.replace(/(status-)/, '')] = value;
+		});
+
+		console.log(results);
+
+		return results;
+	};
 
 	// Print resources status for summary dashboard widget
 	opbg.dashboard_summary_print_status_values = function (target) {
@@ -36,46 +105,52 @@ jQuery(document).ready(function ($) {
 		console.log(data);
 
 		for (var status in data){
-			dashboard_summary_widget.find('.status-table .value.status-' + status).data('status-value', data[status]);
+			dashboard_summary_widget.find('.status-table .value.status-' + status).attr('data-status-value', data[status]).data('status-value', data[status]);
 		}
 		opbg.dashboard_summary_print_status_values();
 	};
 
 	opbg.dashboard_summary_change_visualization_period = function(target){
 
-		var period	= target.data('toggle-option');
+		var pressed_button	= target.data('toggle-option');
 
-		console.log(period);
+		console.log(pressed_button);
 
-		if (period === "range"){
+		if (dashboard_summary_widget.find('.status-period-toggle .active').data('toggle-option') === pressed_button ) return false;
+
+		if (pressed_button === "range"){
 			dashboard_summary_widget.find('.status-data-range').addClass('show');
-			target.parent().find('.btn').toggleClass('active');
+			dashboard_summary_widget.find('.status-period-toggle button').toggleClass('active');
 		}
 		else {
 			dashboard_summary_widget.find('.status-data-range').removeClass('show');
 
-			success = opbg.get_dashboard_summary(period);
+			dashboard_summary_period = "total";
 
-			console.log(success);
+			opbg.get_dashboard_summary();
+
 			dashboard_summary_widget.one('dashboard-summary', function(){
-				target.parent().find('.btn').toggleClass('active');
+				dashboard_summary_widget.find('.status-period-toggle button').toggleClass('active');
 			});
 		}
 
 		return false;
 	};
 
-	opbg.get_dashboard_summary = function(period) {
-		var nonce	= dashboard_summary_widget.find('#dashboard-nonce').val();
-		var buttons = dashboard_summary_widget.find('.status-period-toggle .btn');
-		var spinner		= dashboard_summary_widget.find('.status-period-toggle .spinner');
-		var datepickers = dashboard_summary_widget.find('.status-data-range .datepicker-box');
-		var success	= false;
+	opbg.get_dashboard_summary = function() {
+		var nonce					= dashboard_summary_widget.find('#dashboard-nonce').val();
+        var buttons                 = dashboard_summary_widget.find('.status-period-toggle .btn');
+		var spinner					= dashboard_summary_widget.find('.status-period-toggle .spinner');
+        var datepickers             = dashboard_summary_widget.find('.status-data-range .datepicker-box');
+		var success					= false;
+        are_results_being_called	= true;
 
 		spinner.removeClass('hide');
 
 		buttons.addClass('disabled');
 		datepickers.attr('disabled', true);
+
+		console.log('asking for results with period: '+dashboard_summary_period);
 
 		$.ajax({
 			type:	"post",
@@ -84,7 +159,7 @@ jQuery(document).ready(function ($) {
 				'action':       "dashboard_widget_control",
 				'nonce':        nonce,
 				'button_id':    'get-dashboard-summary',
-				'message':      period
+				'message':      dashboard_summary_period
 			},
 			success: function(response) {
 				if (response.success === true){
@@ -102,10 +177,9 @@ jQuery(document).ready(function ($) {
 				spinner.addClass('hide');
 				buttons.removeClass('disabled');
 				datepickers.attr('disabled', false);
+				are_results_being_called = false;
 			}
 		});
-
-		return success;
 	};
 
 	opbg.dashboard_summary_set_button_state = function(button){
@@ -131,24 +205,48 @@ jQuery(document).ready(function ($) {
 		button.find('i').show();
 	};
 
+	/**** MAIN ****/
+
+	if (dashboard_summary_period !== 'total') {
+
+		var periods = dashboard_summary_period.split(' ');
+		dashboard_summary_widget.find('.status-data-range .datepicker-box[data-box = from]').val(moment(periods[0]).format('YYYY-MM-DD'));
+		dashboard_summary_widget.find('.status-data-range .datepicker-box[data-box = to]').val(moment(periods[1]).format('YYYY-MM-DD'));
+
+		opbg.dashboard_summary_change_visualization_period(dashboard_summary_widget.find('button[data-toggle-option = range]'));
+
+		opbg.set_datepickers(dashboard_summary_widget.find('.status-data-range .datepicker-box[data-box=from]'));
+	}
+	else {
+		dashboard_summary_widget.find('.status-data-range .datepicker-box[data-box = from]').val(moment().subtract('days', 1).format('YYYY-MM-DD'));
+		dashboard_summary_widget.find('.status-data-range .datepicker-box[data-box = to]').val(moment().format('YYYY-MM-DD'));
+
+		opbg.get_dashboard_summary();
+	}
+
+	/**** EVENTS HANDLERS ****/
+
 	$(document).on('heartbeat-send', function(e, data) {
+
+		if (are_results_being_called === true) return; //ajax call on the run
+
+		console.log('heartbeat-send');
+		//if (are_first_results_received === false) return;
 		data.dashboard_heartbeat = 'upgrade_dashboard_summary';
+		data.dashboard_actual_status = opbg.dashboard_summary_get_statistics();
+		data.dashboard_actual_period = dashboard_summary_period;
 	});
 
 	$(document).on( 'heartbeat-tick', function(e, data) {
-		// Only proceed if our data is present
-		if ( ! data.dashboard_summary_data )
-			return;
 
-		opbg.dashboard_summary_upgrade_status_values(data.dashboard_summary_data);
+		if (are_results_being_called === true) return; //ajax call on the run
+
+		console.log('heartbeat-tick');
+
+		if ( data.is_database_changed !== true ) return; //Only proceed if our data is present
+
+		opbg.get_dashboard_summary();
 	});
-
-	//opbg.dashboard_summary_print_status_values();
-
-	dashboard_summary_widget.find('.status-data-range .datepicker-box[data-box = from]').val(moment().subtract('days', 1).format('YYYY-MM-DD'));
-	dashboard_summary_widget.find('.status-data-range .datepicker-box[data-box = to]').val(moment().format('YYYY-MM-DD'));
-
-	opbg.get_dashboard_summary('total');
 
 	dashboard_summary_widget.find('.resources-control').on('click.resources_control', 'button:not(.disabled)', function(){
 		console.log('toggling fetching');
@@ -206,35 +304,9 @@ jQuery(document).ready(function ($) {
 
 	});
 
-	dashboard_summary_widget.find('.status-data-range').on('change.datepicker', '.datepicker-box', function(e){
+	dashboard_summary_widget.find('.status-data-range').on('change.datepicker', '.datepicker-box:not(.disabled)', function(e){
 		console.log(e.target);
 
-		var target = $(e.target);
-		var period = '';
-		var from = moment(dashboard_summary_widget.find('.status-data-range .datepicker-box[data-box = from]').val());
-		var to = moment(dashboard_summary_widget.find('.status-data-range .datepicker-box[data-box = to]').val());
-		var changed = target.data("box");
-
-		console.log(from);
-		console.log(to);
-
-		if (changed === "from"){
-			if (from.isAfter(to)){
-				from = moment(to).subtract('days', 1);
-				dashboard_summary_widget.find('.status-data-range .datepicker-box[data-box = from]').val(from.format('YYYY-MM-DD'));
-			}
-		}
-		if (changed === "to"){
-			if (from.isBefore(from)){
-				to = moment(from).add('days', 1);
-				if (to.isAfter(moment())) to = moment();
-				dashboard_summary_widget.find('.status-data-range .datepicker-box[data-box = to]').val(to.format('YYYY-MM-DD'));
-			}
-		}
-
-		period = from.format('YYYY-MM-DD')+" "+to.format('YYYY-MM-DD');
-
-		opbg.get_dashboard_summary(period);
-
+		opbg.set_datepickers(e.target);
 	});
 });
